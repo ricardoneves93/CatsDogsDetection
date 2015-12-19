5ec249b708729e53026528bfd262c3830430bd1b
Daniel Moreira <danieljlmoreira@gmail.com>
2015-12-19 22:59:54 +0000

    Started project, extracting features from the training set, creating the vocabulary

--- CatsDogsDetection/Main.cpp
+++ CatsDogsDetection/Main.cpp
@@ -1,11 +1,59 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include "Opencv.h"
 
 using namespace cv;
 using namespace std;
 
-int main( int argc, char** argv )
-{
-	cout << "First commit" << endl;
+vector<string> getFilesInDir();
+
+int main( int argc, char** argv ) {
+	
+	string test_dir = "test\\";
+	string dataset_dir = "train\\";
+
+	vector <string> trainImages = getFilesInDir();
+
+	int minHessian = 400;
+	SurfFeatureDetector detector(minHessian);
+	SurfDescriptorExtractor extractor;
+
+	// PTR //
+
+	/* Extracting features from training set thar contains all classes */
+	Mat training_descriptors;
+	for (int i = 0; i < trainImages.size(); i++) {
+		string filepath = dataset_dir + trainImages[i];
+		vector<KeyPoint> keypoints; keypoints.clear();
+		Mat descriptors;
+		Mat img = imread(filepath);
+		detector.detect(img, keypoints);
+		extractor.compute(img, keypoints, descriptors);
+		training_descriptors.push_back(descriptors);
+	}
+
+	/* Creating a vocabulary (bag of words) */
+	int num_clusters = 1000;
+	BOWKMeansTrainer bowtrainer(num_clusters);
+	bowtrainer.add(training_descriptors);
+	Mat vocabulary = bowtrainer.cluster();
+
+
+	return 0;
+}
+
+vector<string> getFilesInDir() {
+	vector<string> trainImages; trainImages.clear();
+	for (int i = 0; i < 12499; i++) {
+		stringstream ss;
+		
+		ss << "dog." << i << ".jpg";
+		string dogImgPath = ss.str();
+		trainImages.push_back(dogImgPath);
+
+		ss.clear();
+		ss << "cat." << i << ".jpg";
+		string carImgPath = ss.str();
+		trainImages.push_back(carImgPath);
+	}
+	return trainImages;
 }
---
+++ CatsDogsDetection/Opencv.h
@@ -0,0 +1,12 @@
+#ifndef OPENCV_H_
+#define OPENCV_H_
+
+#include <opencv/cv.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+
+#endif /* OPENCV_H_ */
